44e9e4dc98489817108c4b3df71cfd28420a4674
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2021-08-31 21:25:05 +0200

    Initial commit

---
+++ Matrix.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace min_error_map
+{
+    class Matrix
+    {
+        private int [,] _matrix;
+        private int _width;
+        private int _height;
+
+        public int [,] matrix
+        {
+            get { return _matrix; }
+        }
+
+        public Matrix(int width, int height)
+        {
+            this._width = width;
+            this._height = height;
+        }
+    }
+}
