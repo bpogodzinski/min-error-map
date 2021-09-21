ab8c77c9db24c8590d214da9a5ec2affb5ba7ba7
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2021-09-21 22:46:05 +0200

    init

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -39,19 +40,47 @@ namespace min_error_map
 
         public static void cmax(int[] row)
         {
+            int cmax = 0;
             // Patrition indexes of ones into list
             var partitions = partitionOnesIntoConsecutiveListOfIndexes(row);
+            //var copyPartitions 
             // Find first longest sequence
             var longestSequence = partitions.OrderByDescending(m => m.Count()).First();
             var indexOfLongestSequenceOnList = partitions.IndexOf(longestSequence);
             // Start calculating from most right
             for (int i = partitions.Count - 1; i >= indexOfLongestSequenceOnList; i--)
             {
-                // Longest sequence is at most right
-                if (partitions[i] == longestSequence)
-                    break;
+                var currentPartition = partitions[i];
 
-                var xd = partitions[i];
+                // Longest sequence is at most right or end
+                if (currentPartition == longestSequence)
+                    break;
+                
+                int leftIndexOnes = longestSequence.Last();
+                int rightIndexOnes = currentPartition.Last();
+                var distance = rightIndexOnes - leftIndexOnes;
+                //{ 1, 1, 1, | 0, 1, 1 |, 0 };
+                var slice = row.Skip(leftIndexOnes + 1).Take(distance).ToArray(); // +1 because we don't want to take "leftover" one
+                var numberOfOnes = slice.Count(x => x == 1);
+                Debug.WriteLine($"{distance - numberOfOnes} <= {numberOfOnes} : {distance - numberOfOnes <= numberOfOnes}");
+                /*
+                 * If change is optimal
+                 * - save cmax
+                 * - calculate left side
+                 * If not
+                 *  - remove one "one"
+                 *  - new iteration
+                 */
+                if(distance - numberOfOnes <= numberOfOnes)
+                {
+                    cmax += distance - numberOfOnes; // fields to change
+                    break;
+                }
+                else
+                {
+                    
+                }
+                
                 Console.WriteLine("xd");
             }
             // Calculate left side
@@ -64,7 +93,7 @@ namespace min_error_map
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             //int[] row = new int[] { 1, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 0 };
-            int[] row = new int[] { 1, 1, 0, 0, 1, 1, 1 };
+            int[] row = new int[] { 1, 1, 1, 0, 1, 1, 0 };
             Program.cmax(row);
 
             //int[,] m = new int[,] { { 0, 0 }, { 0, 0 } };
