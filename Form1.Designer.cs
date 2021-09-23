f7761b92f50183328578bc8912e4d205b066d32a
Bartłomiej Pogodziński <bartlomiej.pogodzinski@gmail.com>
2021-09-23 19:24:07 +0200

    checkpoint start coding

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -33,11 +33,17 @@
             this.mainPanel2 = new System.Windows.Forms.TableLayoutPanel();
             this.mainPanelMatrix = new System.Windows.Forms.TableLayoutPanel();
             this.mainPanelTabuSearch = new System.Windows.Forms.TableLayoutPanel();
-            this.addMatrixButton = new System.Windows.Forms.Button();
             this.mainTextboxMatrix = new System.Windows.Forms.RichTextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.mainPanelAddRandomMatrixButton = new System.Windows.Forms.Button();
+            this.mainPanelAddMatrixFromFileButton = new System.Windows.Forms.Button();
+            this.mainPanelMatrixOptions = new System.Windows.Forms.TableLayoutPanel();
+            this.mainPanelSaveInputButton = new System.Windows.Forms.Button();
             this.mainPanel1.SuspendLayout();
             this.mainPanel2.SuspendLayout();
             this.mainPanelMatrix.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.mainPanelMatrixOptions.SuspendLayout();
             this.SuspendLayout();
             // 
             // mainPanel1
@@ -88,8 +94,9 @@
             // 
             this.mainPanelMatrix.ColumnCount = 1;
             this.mainPanelMatrix.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.mainPanelMatrix.Controls.Add(this.addMatrixButton, 0, 0);
             this.mainPanelMatrix.Controls.Add(this.mainTextboxMatrix, 0, 1);
+            this.mainPanelMatrix.Controls.Add(this.tableLayoutPanel1, 0, 0);
+            this.mainPanelMatrix.Controls.Add(this.mainPanelMatrixOptions, 0, 2);
             this.mainPanelMatrix.Dock = System.Windows.Forms.DockStyle.Fill;
             this.mainPanelMatrix.Location = new System.Drawing.Point(3, 3);
             this.mainPanelMatrix.Name = "mainPanelMatrix";
@@ -114,17 +121,6 @@
             this.mainPanelTabuSearch.Size = new System.Drawing.Size(741, 735);
             this.mainPanelTabuSearch.TabIndex = 1;
             // 
-            // addMatrixButton
-            // 
-            this.addMatrixButton.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.addMatrixButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.addMatrixButton.Location = new System.Drawing.Point(3, 3);
-            this.addMatrixButton.Name = "addMatrixButton";
-            this.addMatrixButton.Size = new System.Drawing.Size(735, 127);
-            this.addMatrixButton.TabIndex = 0;
-            this.addMatrixButton.Text = "Add Matrix";
-            this.addMatrixButton.UseVisualStyleBackColor = true;
-            // 
             // mainTextboxMatrix
             // 
             this.mainTextboxMatrix.Dock = System.Windows.Forms.DockStyle.Fill;
@@ -133,6 +129,67 @@
             this.mainTextboxMatrix.Size = new System.Drawing.Size(735, 443);
             this.mainTextboxMatrix.TabIndex = 1;
             this.mainTextboxMatrix.Text = "";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.mainPanelAddRandomMatrixButton, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.mainPanelAddMatrixFromFileButton, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(735, 127);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // mainPanelAddRandomMatrixButton
+            // 
+            this.mainPanelAddRandomMatrixButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanelAddRandomMatrixButton.Location = new System.Drawing.Point(3, 3);
+            this.mainPanelAddRandomMatrixButton.Name = "mainPanelAddRandomMatrixButton";
+            this.mainPanelAddRandomMatrixButton.Size = new System.Drawing.Size(361, 121);
+            this.mainPanelAddRandomMatrixButton.TabIndex = 0;
+            this.mainPanelAddRandomMatrixButton.Text = "ADD RANDOM MATRIX";
+            this.mainPanelAddRandomMatrixButton.UseVisualStyleBackColor = true;
+            // 
+            // mainPanelAddMatrixFromFileButton
+            // 
+            this.mainPanelAddMatrixFromFileButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanelAddMatrixFromFileButton.Location = new System.Drawing.Point(370, 3);
+            this.mainPanelAddMatrixFromFileButton.Name = "mainPanelAddMatrixFromFileButton";
+            this.mainPanelAddMatrixFromFileButton.Size = new System.Drawing.Size(362, 121);
+            this.mainPanelAddMatrixFromFileButton.TabIndex = 2;
+            this.mainPanelAddMatrixFromFileButton.Text = "ADD MATRIX FROM FILE";
+            this.mainPanelAddMatrixFromFileButton.UseVisualStyleBackColor = true;
+            // 
+            // mainPanelMatrixOptions
+            // 
+            this.mainPanelMatrixOptions.ColumnCount = 2;
+            this.mainPanelMatrixOptions.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainPanelMatrixOptions.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainPanelMatrixOptions.Controls.Add(this.mainPanelSaveInputButton, 0, 0);
+            this.mainPanelMatrixOptions.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanelMatrixOptions.Location = new System.Drawing.Point(3, 585);
+            this.mainPanelMatrixOptions.Name = "mainPanelMatrixOptions";
+            this.mainPanelMatrixOptions.RowCount = 2;
+            this.mainPanelMatrixOptions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainPanelMatrixOptions.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.mainPanelMatrixOptions.Size = new System.Drawing.Size(735, 147);
+            this.mainPanelMatrixOptions.TabIndex = 3;
+            // 
+            // mainPanelSaveInputButton
+            // 
+            this.mainPanelSaveInputButton.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPanelSaveInputButton.Location = new System.Drawing.Point(3, 3);
+            this.mainPanelSaveInputButton.Name = "mainPanelSaveInputButton";
+            this.mainPanelSaveInputButton.Size = new System.Drawing.Size(361, 67);
+            this.mainPanelSaveInputButton.TabIndex = 0;
+            this.mainPanelSaveInputButton.Text = "SAVE INPUT TO FILE";
+            this.mainPanelSaveInputButton.UseVisualStyleBackColor = true;
             // 
             // mainForm
             // 
@@ -146,6 +203,8 @@
             this.mainPanel1.PerformLayout();
             this.mainPanel2.ResumeLayout(false);
             this.mainPanelMatrix.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.mainPanelMatrixOptions.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -156,9 +215,13 @@
         private System.Windows.Forms.Label mainTitleLabel;
         private System.Windows.Forms.TableLayoutPanel mainPanel2;
         private System.Windows.Forms.TableLayoutPanel mainPanelMatrix;
-        private System.Windows.Forms.Button addMatrixButton;
         private System.Windows.Forms.TableLayoutPanel mainPanelTabuSearch;
         private System.Windows.Forms.RichTextBox mainTextboxMatrix;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button mainPanelAddRandomMatrixButton;
+        private System.Windows.Forms.Button mainPanelAddMatrixFromFileButton;
+        private System.Windows.Forms.TableLayoutPanel mainPanelMatrixOptions;
+        private System.Windows.Forms.Button mainPanelSaveInputButton;
     }
 }
 
